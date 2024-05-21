f76a40026addf8bcc543ea1cb437726e0811057d
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-05-21 22:56:47 +0200

    Update app.py

--- free_one_api/impls/app.py
+++ free_one_api/impls/app.py
@@ -79,8 +79,12 @@ log_colors_config = {
 default_config = {
     "1-documentation": "see at https://github.com/RockChinQ/free-one-api",
     "database": {
-        "type": "sqlite",
-        "path": "./data/free_one_api.db",
+        "type": "mysql",
+        "host": os.environ.get("DB_HOST"),
+        "port": int(os.environ.get("DB_PORT", 3306)),
+        "user": os.environ.get("DB_USER"),
+        "password": os.environ.get("DB_PASSWORD"),
+        "database": os.environ.get("DB_NAME"),
     },
     "watchdog": {
         "heartbeat": {
