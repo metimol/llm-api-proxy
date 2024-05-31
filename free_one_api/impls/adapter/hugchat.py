d957f66d4279b2a94e532961b2fb686856793aa1
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-05-31 23:36:17 +0200

    Update hugchat.py

--- free_one_api/impls/adapter/hugchat.py
+++ free_one_api/impls/adapter/hugchat.py
@@ -123,7 +123,7 @@ Please refer to https://github.com/Soulter/hugging-chat-api
                 yield response.Response(
                     id=random_int,
                     finish_reason=response.FinishReason.NULL,
-                    normal_message=resp['token'],
+                    normal_message=resp,
                     function_call=None
                 )
         except Exception as e:
