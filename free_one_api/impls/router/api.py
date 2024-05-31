705fd371e154f8bb6328ddca35b4b13e8ef1401a
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-05-31 20:49:19 +0200

    Update api.py

--- free_one_api/impls/router/api.py
+++ free_one_api/impls/router/api.py
@@ -51,7 +51,7 @@ class WebAPIGroup(routergroup.APIGroup):
 
                 for chan in chan_list:
                     chan_obj = channel.Channel.dump_channel(chan)
-                    supported_models = chan_obj["adapter"]["config"].get("models", None)
+                    supported_models = chan_obj["adapter"]["config"]["models"]
                     unique_models.update(supported_models)
 
                 return quart.jsonify({
