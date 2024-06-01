7b9a3f0c72a2068c687f267f269c3ed84d92d3bd
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-06-01 21:24:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
 import sys
 import asyncio
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 if not os.path.exists('./data'):
     os.mkdir('./data')
@@ -12,10 +9,9 @@ from free_one_api.impls import app
 
 def main():
     loop = asyncio.get_event_loop()
-    
+
     application = loop.run_until_complete(app.make_application("./data/config.yaml"))
 
-    logging.getLogger().setLevel(application.logging_level)
     loop.run_until_complete(application.run())
 
 if __name__ == "__main__":
