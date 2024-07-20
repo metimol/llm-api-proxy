328c00408000c54928cc5b7dc941ff0c90e38ba5
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-07-20 22:49:37 +0200

    Update deepinfra.py

--- free_one_api/impls/adapter/deepinfra.py
+++ free_one_api/impls/adapter/deepinfra.py
@@ -51,6 +51,25 @@ class DeepinfraAdapter(llm.LLMLibAdapter):
     def supported_path(cls) -> str:
         return "/v1/chat/completions"
 
+    def get_headers(self, user_agent: str, content_length: int):
+        return {
+            'Authority': 'api.deepinfra.com',
+            'Host': 'api.deepinfra.com',
+            'User-Agent': f'{user_agent}',
+            'Accept': 'text/event-stream',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://deepinfra.com/',
+            'Content-Type': 'application/json',
+            'X-Deepinfra-Source': 'web-page',
+            'Content-Length': f'{content_length}',
+            'Origin': 'https://deepinfra.com',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site'
+        }
+
     def __init__(self, config: dict, eval: evaluation.AbsChannelEvaluation):
         self.config = config
         self.eval = eval
