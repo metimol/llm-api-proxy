ad0adad06198df3ca2f0981b555cdb0d8676fc52
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-07-22 00:04:54 +0200

    Update deepinfra.py

--- free_one_api/impls/adapter/deepinfra.py
+++ free_one_api/impls/adapter/deepinfra.py
@@ -2,7 +2,6 @@ import typing
 import random
 import httpx
 import ujson
-
 from ...models import adapter
 from ...models.adapter import llm
 from ...entities import request, response
@@ -10,29 +9,36 @@ from ...models.channel import evaluation
 
 @adapter.llm_adapter
 class DeepinfraAdapter(llm.LLMLibAdapter):
+    NAME = "Deepinfra/Deepinfra-API"
+    DESCRIPTION = "Use Deepinfra/Deepinfra-API to access official deepinfra API."
+    SUPPORTED_MODELS = [
+        'deepinfra/airoboros-70b', 'cognitivecomputations/dolphin-2.6-mixtral-8x7b', 'openchat/openchat-3.6-8b',
+        'mistralai/Mixtral-8x22B-Instruct-v0.1', 'google/codegemma-7b-it', 'microsoft/WizardLM-2-8x22B',
+        'HuggingFaceH4/zephyr-orpo-141b-A35b-v0.1', 'databricks/dbrx-instruct', 'mistralai/Mistral-7B-Instruct-v0.3',
+        'meta-llama/Llama-2-70b-chat-hf', 'mistralai/Mixtral-8x7B-Instruct-v0.1', 'meta-llama/Meta-Llama-3-8B-Instruct',
+        'mistralai/Mixtral-8x22B-v0.1', '01-ai/Yi-34B-Chat', 'meta-llama/Llama-2-7b-chat-hf', 'nvidia/Nemotron-4-340B-Instruct',
+        'meta-llama/Llama-2-13b-chat-hf', 'google/gemma-1.1-7b-it', 'Qwen/Qwen2-72B-Instruct', 'microsoft/Phi-3-medium-4k-instruct',
+        'Sao10K/L3-70B-Euryale-v2.1', 'cognitivecomputations/dolphin-2.9.1-llama-3-70b', 'meta-llama/Meta-Llama-3-70B-Instruct', 
+        'lizpreciatior/lzlv_70b_fp16_hf', 'microsoft/WizardLM-2-7B', 'llava-hf/llava-1.5-7b-hf', 'google/gemma-2-9b-it', 'google/gemma-2-27b-it'
+    ]
+    CONFIG_COMMENT = "For this website you don't need any key"
+    SUPPORTED_PATH = "/v1/chat/completions"
+    BASE_URL = "https://api.deepinfra.com/v1/openai/chat/completions"
+
     @classmethod
     def name(cls) -> str:
-        return "Deepinfra/Deepinfra-API"
+        return cls.NAME
 
     @classmethod
     def description(cls) -> str:
-        return "Use Deepinfra/Deepinfra-API to access official deepinfra API."
+        return cls.DESCRIPTION
 
     def supported_models(self) -> list[str]:
-        return [
-            'deepinfra/airoboros-70b', 'cognitivecomputations/dolphin-2.6-mixtral-8x7b', 'openchat/openchat-3.6-8b',
-            'mistralai/Mixtral-8x22B-Instruct-v0.1', 'google/codegemma-7b-it', 'microsoft/WizardLM-2-8x22B',
-            'HuggingFaceH4/zephyr-orpo-141b-A35b-v0.1', 'databricks/dbrx-instruct', 'mistralai/Mistral-7B-Instruct-v0.3',
-            'meta-llama/Llama-2-70b-chat-hf', 'mistralai/Mixtral-8x7B-Instruct-v0.1', 'meta-llama/Meta-Llama-3-8B-Instruct',
-            'mistralai/Mixtral-8x22B-v0.1', '01-ai/Yi-34B-Chat', 'meta-llama/Llama-2-7b-chat-hf', 'nvidia/Nemotron-4-340B-Instruct',
-            'meta-llama/Llama-2-13b-chat-hf', 'google/gemma-1.1-7b-it', 'Qwen/Qwen2-72B-Instruct', 'microsoft/Phi-3-medium-4k-instruct',
-            'Sao10K/L3-70B-Euryale-v2.1', 'cognitivecomputations/dolphin-2.9.1-llama-3-70b', 'meta-llama/Meta-Llama-3-70B-Instruct', 
-            'lizpreciatior/lzlv_70b_fp16_hf', 'microsoft/WizardLM-2-7B', 'llava-hf/llava-1.5-7b-hf', 'google/gemma-2-9b-it', 'google/gemma-2-27b-it'
-        ]
-    
+        return self.SUPPORTED_MODELS
+
     def function_call_supported(self) -> bool:
         return False
-    
+
     def stream_mode_supported(self) -> bool:
         return True    
 
@@ -41,17 +47,16 @@ class DeepinfraAdapter(llm.LLMLibAdapter):
 
     @classmethod
     def config_comment(cls) -> str:
-        return "For this website you don't need any key"
+        return cls.CONFIG_COMMENT
 
     @classmethod
     def supported_path(cls) -> str:
-        return "/v1/chat/completions"
+        return cls.SUPPORTED_PATH
 
     def __init__(self, config: dict, eval: evaluation.AbsChannelEvaluation):
         self.config = config
         self.eval = eval
-        self.url = "https://api.deepinfra.com/v1/openai/chat/completions"
-        self.use_proxy = False
+        self.use_proxy = config.get('use_proxy', False)
 
     async def test(self) -> typing.Union[bool, str]:
         data = {
@@ -64,20 +69,13 @@ class DeepinfraAdapter(llm.LLMLibAdapter):
             "presence_penalty": 0.0,
             "frequency_penalty": 0.0
         }
-        headers = {
-            'X-Deepinfra-Source': 'web-page',
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
-        }
-        return await self.make_request(self.url, data, headers, is_test=True)
+        headers = self._get_headers()
+        return await self.make_request(self.BASE_URL, data, headers, is_test=True)
 
     async def query(self, req: request.Request) -> typing.AsyncGenerator[response.Response, None]:
-        messages = req.messages
-        model = req.model
-        random_int = random.randint(0, 1000000000)
-
         data = {
-            "messages": messages,
-            "model": model,
+            "messages": req.messages,
+            "model": req.model,
             "stream": True,
             "temperature": 0.7,
             "max_tokens": 15000,
@@ -86,75 +84,39 @@ class DeepinfraAdapter(llm.LLMLibAdapter):
             "presence_penalty": 0.0,
             "frequency_penalty": 0.0
         }
+        headers = self._get_headers()
+        random_int = random.randint(0, 1000000000)
 
-        headers = {
-            'X-Deepinfra-Source': 'web-page',
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
-        }
-
-        async with httpx.AsyncClient() as client:
-            async with client.stream("POST", self.url, json=data, headers=headers) as result:
-                if result.status_code == 200:
-                    async for line in result.aiter_lines():
-                        if line:
-                            if line.startswith("data: "):
-                                line = line[6:]
-                            if line == "[DONE]":
-                                yield response.Response(
-                                    id=random_int,
-                                    finish_reason=response.FinishReason.STOP,
-                                    normal_message="",
-                                    function_call=None
-                                )
-                                break
-                            try:
-                                chunk = ujson.loads(line)
-                                if chunk.get("choices") and chunk["choices"][0].get("delta", {}).get("content"):
-                                    text = chunk["choices"][0]["delta"]["content"]
-                                    yield response.Response(
-                                        id=random_int,
-                                        finish_reason=response.FinishReason.NULL,
-                                        normal_message=text,
-                                        function_call=None
-                                    )
-                            except ValueError as e:
-                                raise ValueError(f"JSON decoding error: {e}\nLine content: {line}")
-                else:
-                    raise Exception(f"HTTP request failed with status code: {result.status_code}")
+        async with await self.make_request(self.BASE_URL, data, headers) as result:
+            if result.status_code == 200:
+                async for line in result.aiter_lines():
+                    if line:
+                        yield from self._process_line(line, random_int)
+            else:
+                raise Exception(f"HTTP request failed with status code: {result.status_code}")
 
     async def make_request(self, url, data, headers, is_test=False):
-        if self.use_proxy:
-            for _ in range(30):
-                try:
-                    proxy_address = await self.get_working_proxy()
-                    async with httpx.AsyncClient(proxies=proxy_address) as client:
-                        if is_test:
-                            response = await client.post(url, json=data, headers=headers)
-                            response.raise_for_status()
-                            return True, ""
-                        else:
-                            return await client.stream("POST", url, json=data, headers=headers)
-                except (httpx.HTTPStatusError, httpx.RequestError) as e:
-                    continue
-            return False, "Could not find a working proxy after multiple retries."
-        else:
-            async with httpx.AsyncClient() as client:
-                if is_test:
-                    response = await client.post(url, json=data, headers=headers)
-                    response.raise_for_status()
-                    return True, ""
-                else:
-                    return await client.stream("POST", url, json=data, headers=headers)
+        client_kwargs = {'proxies': await self.get_working_proxy()} if self.use_proxy else {}
+        
+        async with httpx.AsyncClient(**client_kwargs) as client:
+            if is_test:
+                response = await client.post(url, json=data, headers=headers)
+                response.raise_for_status()
+                return True, ""
+            else:
+                return await client.stream("POST", url, json=data, headers=headers)
 
     async def get_proxy_list(self):
         proxy_url = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
         async with httpx.AsyncClient() as client:
             response = await client.get(proxy_url)
             response.raise_for_status()
-            proxy_list = response.text.strip().split("\n")
-        return proxy_list
+            return response.text.strip().split("\n")
 
     async def get_working_proxy(self):
+        if not self.use_proxy:
+            return None
+        
         proxy_list = await self.get_proxy_list()
         for proxy in proxy_list:
             try:
@@ -166,3 +128,33 @@ class DeepinfraAdapter(llm.LLMLibAdapter):
             except (httpx.HTTPStatusError, httpx.RequestError):
                 continue
         raise Exception("Could not find a working proxy after multiple retries.")
+
+    def _get_headers(self):
+        return {
+            'X-Deepinfra-Source': 'web-page',
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+        }
+
+    def _process_line(self, line: str, random_int: int):
+        if line.startswith("data: "):
+            line = line[6:]
+        if line == "[DONE]":
+            yield response.Response(
+                id=random_int,
+                finish_reason=response.FinishReason.STOP,
+                normal_message="",
+                function_call=None
+            )
+        else:
+            try:
+                chunk = ujson.loads(line)
+                if chunk.get("choices") and chunk["choices"][0].get("delta", {}).get("content"):
+                    text = chunk["choices"][0]["delta"]["content"]
+                    yield response.Response(
+                        id=random_int,
+                        finish_reason=response.FinishReason.NULL,
+                        normal_message=text,
+                        function_call=None
+                    )
+            except ValueError as e:
+                raise ValueError(f"JSON decoding error: {e}\nLine content: {line}")
